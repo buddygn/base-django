dcf4996a27731c993941492069403a180f7049c2
geisonnervo <p4fkt0of>
2021-07-22 14:56:09 -0300

    teste deploy 0.0.00.018

--- base/settings.py
+++ base/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY', cast=str)
-SECRET_KEY = '1#9(ptjjh!nq!i02ufv#)!l$^i_k16$p))v33tw@g)+!#o&d(c'
+SECRET_KEY = config('SECRET_KEY', cast=str)
+# SECRET_KEY = '1#9(ptjjh!nq!i02ufv#)!l$^i_k16$p))v33tw@g)+!#o&d(c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
