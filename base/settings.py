03a9c61127316102d83f8b3c55a27aae828efb03
geisonnervo <p4fkt0of>
2021-07-22 11:25:31 -0300

    teste deploy 0.0.00.007

--- base/settings.py
+++ base/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = config('SECRET_KEY', cast=str)
-SECRET_KEY = '1#9(ptjjh!nq!i02ufv#)!l$^i_k16$p))v33tw@g)+!#o&d(c'
+SECRET_KEY = config('SECRET_KEY', cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
