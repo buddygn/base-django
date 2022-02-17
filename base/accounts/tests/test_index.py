f90a7ec85b4c29a13571bbed29281fba6fcc2032
buddygn <buddygn@gmail.com>
2022-02-17 15:46:11 -0300

    configuraçoes ci 18

--- base/accounts/tests/test_index.py
+++ base/accounts/tests/test_index.py
@@ -1,10 +1,10 @@
 import pytest
-from django.urls import reverse
+from django.urls import reverse_lazy
 
 
 @pytest.fixture
 def resp(client, db):
-    resp = client.get(reverse('BASE:home'))
+    resp = client.get(reverse_lazy('base:home'))
     return resp
 
 
--- base/core/urls.py
+++ base/core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 
 from base.core.views import home
 
-APP_NAME = 'base'
+app_name = 'base'
 urlpatterns = [path('', home, name='home')]
